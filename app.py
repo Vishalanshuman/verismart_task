aa8921067bd2606039e64741a2ce4418976d1630
Vishal Bhardwaj <vishalanshuman@gmail.com>
2024-02-19 21:39:13 +0530

    initial push 002

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ def get_group_permissions(group_id:int):
     if group_id not in group_ids.values():
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Group not found")
     if group_permissions[group_id] is None:
-        raise HTTPException(status_code=404, detail="Permissions for the group have not been set yet")
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Permissions for the group have not been set yet")
     permission=group_permissions[group_id]
     return {"group_id":group_id,"group_permission":permission}
 
@@ -55,9 +55,9 @@ def set_group_permission(group_id: int, permission: GroupPermission):
 @app.put("/groups/{group_id}/permissions")
 def update_group_permission(group_id: int, permission: GroupPermission):
     if group_id not in group_ids.values():
-        raise HTTPException(status_code=404, detail="Group not found")
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Group not found")
     if group_permissions[group_id] is None:
-        raise HTTPException(status_code=404, detail="Permissions for the group have not been set yet")
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Permissions for the group have not been set yet")
     group_permissions[group_id] = permission
     return {
         "message": f"Permissions updated successfully for group with ID {group_id}",
@@ -67,8 +67,8 @@ def update_group_permission(group_id: int, permission: GroupPermission):
 @app.delete("/groups/{group_id}/permissions")
 def delete_group_permission(group_id: int):
     if group_id not in group_ids.values():
-        raise HTTPException(status_code=404, detail="Group not found")
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Group not found")
     if group_permissions[group_id] is None:
-        raise HTTPException(status_code=404, detail="Permissions for the group have not been set yet")
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Permissions for the group have not been set yet")
     group_permissions[group_id]=None
     return {"message": f"Permissions deleted successfully for group with ID {group_id}"}
